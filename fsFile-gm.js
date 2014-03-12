d52616077997b842dec34d6b504ce8672f6ceb3c
Eric Dobbertin <eric@dairystatedesigns.com>
2014-03-12 00:36:54 -0500

    updates to support streaming conversion; still WIP

--- fsFile-gm.js
+++ fsFile-gm.js
@@ -1,33 +1,35 @@
 //wrap gm() object with an object that exposes the same methods, with the addition of a
 //.save() method that overwrites the FS.File's .buffer with the result
 
-var path = Npm.require('path');
-var Future = Npm.require(path.join('fibers', 'future'));
 var gm = Npm.require('gm');
 
 if (typeof FS.File !== "undefined") {
 
+  /**
+   * @param FS.File.prototype.gm
+   * @public
+   * @param {Object} options
+   * @param {String} [options.type] Content type with which to save
+   * @param {Stream} [options.stream] Write stream to pipe results to. If not set, will pipe to write stream for the 
+   */
   FS.File.prototype.gm = function(options) {
     var self = this;
+    options = options || {};
     var subGM = gm.subClass(_.extend({}, options, {fsFile: self}));
-    return subGM(self.getBuffer(), self.name);
+    return subGM(self.createReadStream(options.store), self.name);
   };
   
-  // Filename and type are optional
-  gm.prototype.save = function(type) {
+  /**
+   * @param save
+   * @public
+   * @param {Object} options
+   * @param {String} [options.type] Content type with which to save
+   * @param {Stream} [options.stream] Write stream to pipe results to. If not set, will pipe to write stream for the same store we read from.
+   */
+  gm.prototype.save = function(options) {
     var self = this;
-    var fut = new Future();
-    
-    var callback = Meteor.bindEnvironment(function(err, buffer) {
-      if (err)
-        throw err;
-      self._options.fsFile.setDataFromBuffer(buffer, type);
-      fut.return(self);
-    }, function (err) {
-      throw err;
-    });
-    this.toBuffer(callback);
-    
-    return fut.wait();
+    options = options || {};
+    var writeStream = self._options.fsFile.createWriteStream(options.store);
+    self.stream(options.type).pipe(writeStream);
   };
 }
