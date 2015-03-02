e8067909545ed4d18f314301cef257c4f92cd938
Eric Dobbertin <eric@dairystatedesigns.com>
2015-03-02 10:31:28 -0600

    bump gm and package version

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'cfs:graphicsmagick',
-  version: '0.0.17',
+  version: '0.0.18',
   summary: "Adds `gm` to scope with the ability to perform GraphicsMagick or ImageMagick manipulation",
   git: "https://github.com/CollectionFS/Meteor-cfs-graphicsmagick.git"
 });
 
 Npm.depends({
-  gm: "1.14.2"
+  gm: "1.17.0"
 });
 
 //also requires that you install the ImageMagick
